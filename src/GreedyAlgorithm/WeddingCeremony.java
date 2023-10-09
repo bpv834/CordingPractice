27c5c9424a68d72da7f465f55448881b1c37fca8
ho <yungjoon2@naver.com>
2023-10-09 19:41:17 +0900

    23.10.09 Comporator인터페이스 사용, String.compareTo(), getter,setter

---
+++ src/GreedyAlgorithm/WeddingCeremony.java
@@ -0,0 +1,74 @@
+package GreedyAlgorithm;
+
+import java.util.*;
+
+public class WeddingCeremony {
+    static Scanner sc = new Scanner(System.in);
+    static int n = sc.nextInt();
+    static int[][] arr = new int[n][2];
+    static ArrayList<Time> times = new ArrayList<>();
+    static int answer = Integer.MIN_VALUE;
+    static int cnt=0;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2; j++) {
+                if (j == 0) {
+                    Time tmp1 = new Time(arr[i][j], "S");
+                    times.add(tmp1);
+                } else if (j == 1) {
+                    Time tmp2 = new Time(arr[i][j], "E");
+                    times.add(tmp2);
+                }
+            }
+        }
+        Collections.sort(times);
+        for (Time t : times) {
+            System.out.println(t.getTime() + " " + t.getState());
+            if (t.state == "S") {
+                answer = Math.max(++cnt, answer);
+            }
+            else {
+                answer = Math.max(--cnt, answer);
+            }
+        }
+        System.out.println(answer);
+
+    }
+    static class Time implements Comparable<Time> {
+        private int time;
+        private String state;
+
+        public Time(int time, String state) {
+            this.time = time;
+            this.state = state;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        @Override
+        public int compareTo(Time other) {
+            // 시간을 기준으로 오름차순 정렬
+            if (this.time - other.time == 0) {
+//                return Integer.compare(this.state - other.state);
+                return this.state.compareTo(other.state);
+            }
+            else {
+                return this.time - other.time;
+            }
+        }
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,14 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
-
     static Scanner sc = new Scanner(System.in);
     static int n = sc.nextInt();
-    static int arr[][] = new int[n][2];
-    static int tmp[] = new int[2];
-    static int cnt=0;
-
-    static void arrList(int arr[][]) {
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                if (o1[1] == o2[1]) {
-                    return o1[0]-o2[0];
-                } else {
-                    return o1[1] - o2[1];
-                }
-            }
-        });
-    }
+    static int[][] arr = new int[n][2];
+    static ArrayList<Time> times = new ArrayList<>();
+    static int answer = Integer.MIN_VALUE;
+    static int cnt = 0;
 
     public static void main(String[] args) {
         for (int i = 0; i < n; i++) {
@@ -27,19 +16,54 @@ public class Main {
                 arr[i][j] = sc.nextInt();
             }
         }
-
-        arrList(arr);
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < 1; j++) {
-                if (arr[i][0] >= tmp[1]) {
-                    ++cnt;
-                    tmp[0] = arr[i][0];
-                    tmp[1] = arr[i][1];
+            for (int j = 0; j < 2; j++) {
+                if (j == 0) {
+                    Time tmp1 = new Time(arr[i][j], "S");
+                    times.add(tmp1);
+                } else if (j == 1) {
+                    Time tmp2 = new Time(arr[i][j], "E");
+                    times.add(tmp2);
                 }
             }
         }
-        System.out.println(cnt);
+        Collections.sort(times);
+        for (Time t : times) {
+            if (t.state == "S") {
+                answer = Math.max(++cnt, answer);
+            } else {
+                answer = Math.max(--cnt, answer);
+            }
+        }
+        System.out.println(answer);
+
+    }
+
+    static class Time implements Comparable<Time> {
+        private int time;
+        private String state;
+
+        public Time(int time, String state) {
+            this.time = time;
+            this.state = state;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        public String getState() {
+            return state;
+        }
+
+        @Override
+        public int compareTo(Time other) {
+            // 시간을 기준으로 오름차순 정렬
+            if (this.time - other.time == 0) {
+                return this.state.compareTo(other.state);
+            } else {
+                return this.time - other.time;
+            }
+        }
     }
 }
-
-
