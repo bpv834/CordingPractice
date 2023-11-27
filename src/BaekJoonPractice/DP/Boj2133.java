114ae68ae87583dd4bee85c0f2353c3bc795d2b2
ho <yungjoon2@naver.com>
2023-11-27 18:45:46 +0900

    23.11.27 DP 2133 타일 채우기

--- src/BaekJoonPractice/DP/Boj2133.java
+++ src/BaekJoonPractice/DP/Boj2133.java
@@ -1,28 +1,27 @@
-
+package BaekJoonPractice.DP;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class Main {
+public class Boj2133 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int dp[] = new int[n + 1];
 
         dp[0] = 1;
-        if (n == 2) dp[n] = 3;
-        if (n >= 4) {
-            dp[2] = 3;
-            for (int i = 2; i <= n; i += 2) {
-                dp[i] = dp[i - 2] * dp[2];
-                for (int j = i - 4; j >= 0; j -= 2) {
-                    dp[i] += dp[j] * 2;
-                }
+        dp[1] = 0;
+        dp[2] = 3;
+        dp[3] = 0;
+
+        for (int i = 4; i <= n; i += 2) {
+            dp[i] = dp[i - 2] * dp[2];
+            for (int j = i - 4; j >= 0; j -= 2) {
+                dp[i] += dp[j] * 2;
             }
         }
-        if (n % 2 != 0) System.out.println(0);
-        else System.out.println(dp[n]);
+        System.out.println(Arrays.toString(dp));
     }
 }
