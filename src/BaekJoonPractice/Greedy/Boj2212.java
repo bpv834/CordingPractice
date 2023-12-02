a268af81a2ecf273a6727607db02cee68108962b
ho <yungjoon2@naver.com>
2023-12-02 14:58:24 +0900

    23.11.30 greedy 2212

--- src/BaekJoonPractice/Greedy/Boj2212.java
+++ src/BaekJoonPractice/Greedy/Boj2212.java
@@ -1,3 +1,4 @@
+package BaekJoonPractice.Greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -5,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj2212 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -23,7 +24,8 @@ public class Main {
             diff[i] = arr[i + 1] - arr[i];
         }
         Arrays.sort(diff);
-
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(diff));
         for (int i = 0; i < n  - k; i++) {
             ans+=diff[i];
         }
