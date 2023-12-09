9746e45292ae9847119ad8669fd0462e7d3da1dc
ho <yungjoon2@naver.com>
2023-12-09 20:32:58 +0900

    23.12.09 greedy Boj1459

--- src/BaekJoonPractice/Greedy/Boj1459.java
+++ src/BaekJoonPractice/Greedy/Boj1459.java
@@ -1,10 +1,11 @@
+package BaekJoonPractice.Greedy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj1459 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
@@ -39,7 +40,7 @@ public class Main {
         } else {
             ans = w * (x + y);
         }
-        System.out.println(ans);
+         System.out.println(ans);
 
     }
 }
