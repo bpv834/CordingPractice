887c1d9ac928314a8c945b172bca74f51813e648
ho <yungjoon2@naver.com>
2023-12-12 21:35:22 +0900

    23.12.12 Search Boj 5014

--- src/BaekJoonPractice/Search/Boj5014.java
+++ src/BaekJoonPractice/Search/Boj5014.java
@@ -1,3 +1,4 @@
+package BaekJoonPractice.Search;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,10 +7,10 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj5014 {
     static BufferedReader br;
     static int f, s, g, u, d;
-    static int ch[] = new int[1000001];
+    static int ch[] = new int[100001];
     static boolean flag = false;
     static Queue<Edge> q = new LinkedList<>();
 
@@ -19,7 +20,7 @@ public class Main {
             Edge nowEdge = q.poll();
             int nowVtx = nowEdge.vtx;
             int nowL = nowEdge.L;
-            if (nowVtx > f || nowVtx < 1) continue;
+            if (nowVtx < 1 || nowVtx > f) continue;
             if (ch[nowVtx] == 1) continue;
             ch[nowVtx] = 1;
             if (nowVtx == g) {
@@ -29,8 +30,8 @@ public class Main {
             } else {
                 int nu = nowVtx + u;
                 int nd = nowVtx - d;
-                if (nu >= 1 && nu <= f) q.add(new Edge(nu, nowL + 1));
-                if (nd >= 1 && nd <= f) q.add(new Edge(nd, nowL + 1));
+                if (nu >= 0 && nu <= 1000000) q.add(new Edge(nu, nowL + 1));
+                if (nd >= 0 && nd <= 1000000) q.add(new Edge(nd, nowL + 1));
             }
         }
         if (!flag) System.out.println("use the stairs");
