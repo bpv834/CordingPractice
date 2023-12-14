4833c90e9acb7b56e331064b2397c1267273a7c9
ho <yungjoon2@naver.com>
2023-12-14 19:57:02 +0900

    23.12.14 Dp Boj 1890

--- src/BaekJoonPractice/DP/Boj1890.java
+++ src/BaekJoonPractice/DP/Boj1890.java
@@ -1,9 +1,11 @@
+package BaekJoonPractice.DP;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj1890 {
     static BufferedReader br;
     static int n;
 
